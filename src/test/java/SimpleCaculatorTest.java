f16a696c9dbf7663b17a305dfec9bb46eeeb88ff
Insang Chung <insang@hansung.ac.kr>
2016-03-31 20:10:26 +0900

    kkkk

--- src/test/java/SimpleCaculatorTest.java
+++ src/test/java/SimpleCaculatorTest.java
@@ -18,6 +18,13 @@ public class SimpleCaculatorTest {
 		calc.sub(10, 20);
 		assertEquals(-10, calc.getResult());
 	}
+	
+	@Test
+	public void testAdd() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.add(10, 20);
+		assertEquals(30, calc.getResult());
+	}
 	@Test
 	public void testInc() {
 		SimpleCalculator calc = new SimpleCalculator();
