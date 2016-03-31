9793b9fa7ff3656bdca01e28708f380093dece62
Insang Chung <insang@hansung.ac.kr>
2016-03-31 19:35:39 +0900

    ok1

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,20 @@
+
+
+public class SimpleCalculator {
+	private int res=0;
+	
+	public void add(int x, int y) {
+		res=x+y;
+	}
+	
+	public void sub(int x, int y) {
+		res=x-y;
+	}
+	
+	public void inc(int d) {
+		res+=d;
+	}
+	public int getResult() {
+		return res;
+	}
+}
